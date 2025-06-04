797ebe3de56813ac0c99beddfb13bce273181864
Riyan <griyan115@gmail.com>
2025-06-04 21:14:33 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import './globals.css';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen bg-black items-center justify-center ">
+      <h1 className="text-4xl font-bold text-blue-900 text-center">
+        Tailwind CSS v3.4 with Next.js + TS + pnpm 🎉
+      </h1>
+    </div>
+  )
+}
