5fc5ed188cb9d0b26ca1d5ab7ac2c365f7e1affd
Riyan <griyan115@gmail.com>
2025-06-12 22:54:52 +0530

    invoice generation

--- app/api/invoice/[invoiceId]/route.ts
+++ app/api/invoice/[invoiceId]/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/app/utils/db";
 import { NextResponse } from "next/server";
+import { jsPDF } from "jspdf";
 
 export async function GET(
   request: Request,
@@ -35,6 +36,40 @@ export async function GET(
   });
 
   if (!data) {
-   return NextResponse.json({ error: "Invoice not found" }, { status: 404 });
+    return NextResponse.json({ error: "Invoice not found" }, { status: 404 });
   }
+
+  const pdf = new jsPDF({
+    orientation: "portrait",
+    unit: "mm",
+    format: "a4",
+  });
+
+  pdf.setFont("helvetica");
+  // header
+  pdf.setFontSize(24);
+  pdf.text(data.invoiceName, 20, 20);
+
+  //From
+  pdf.setFontSize(12);
+  pdf.text("From", 20, 40);
+  pdf.setFontSize(10);
+  pdf.text([data.fromName, data.fromEmail, data.fromAddress], 20, 45);
+  //To
+  pdf.setFontSize(12);
+  pdf.text("Bill to", 20, 70);
+  pdf.setFontSize(10);
+  pdf.text([data.clientName, data.clientEmail, data.clientAddress], 20, 75);
+
+  //generatign pdf as buffer.
+
+  const pdfBuffer = Buffer.from(pdf.output("arraybuffer"));
+
+  //return pdf as download
+  return new NextResponse(pdfBuffer, {
+    headers: {
+      "Content-type": "application/pdf",
+      "Content-Disposition": "inline",
+    },
+  });
 }
