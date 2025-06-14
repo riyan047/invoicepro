e17607c16198fe454edc4c9b400f6ccf5c330aeb
Riyan <griyan115@gmail.com>
2025-06-14 22:28:19 +0530

    reminder email

--- app/api/email/[invoiceId]/route.ts
+++ app/api/email/[invoiceId]/route.ts
@@ -1,6 +1,49 @@
+import prisma from "@/app/utils/db";
+import { requierUser } from "@/app/utils/hooks";
+import { emailClient } from "@/app/utils/mailtrap";
+import { NextResponse } from "next/server";
+
 export async function POST(
   request: Request,
   { params }: { params: Promise<{ invoiceId: string }> }
 ) {
-  return;
+  try {
+    const session = await requierUser();
+
+    const { invoiceId } = await params;
+
+    const invoiceData = await prisma.invoice.findUnique({
+      where: {
+        id: invoiceId,
+        userId: session.user?.id,
+      },
+    });
+    if (!invoiceData) {
+      return NextResponse.json({ error: "Invoice not found" }, { status: 404 });
+    }
+
+    const sender = {
+      email: "InvoicePro@riyang.co.in",
+      name: "InvoicePro",
+    };
+    const recipients = [
+      {
+        email: invoiceData.clientEmail,
+      },
+    ];
+
+    emailClient.send({
+      from: sender,
+      to: recipients,
+      subject: "Reminder Invoice Payment",
+      text: "Hey you forgot to pay the invoice.",
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to send email reminder" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({ success: "true" });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,16 @@ import './globals.css';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-black items-center justify-center ">
-      <h1 className="text-4xl font-bold text-blue-900 text-center">
-        Tailwind CSS v3.4 with Next.js + TS + pnpm 🎉
-      </h1>
+    // <div className="flex min-h-screen bg-black items-center justify-center ">
+    //   <h1 className="text-4xl font-bold text-blue-900 text-center">
+    //     Tailwind CSS v3.4 with Next.js + TS + pnpm 🎉
+    //   </h1>
+    // </div>
+    <div className="container">
+      <div className="text-sm bg-red-500 md:bg-green-500 lg:bg-blue-600 md:text-lg lg:text-2xl">
+        Responsive Text
+      </div>
     </div>
+
   )
 }
